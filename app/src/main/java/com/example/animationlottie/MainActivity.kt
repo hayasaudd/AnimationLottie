7f5e7f89e6574fb6e05b7c9d0bfb8a3fed1bc358
haya <haya.saud.ruh.k@tuwaiq.edu.sa>
2021-12-14 09:23:53 +0300

    Add login-logout

--- app/src/main/java/com/example/animationlottie/MainActivity.kt
+++ app/src/main/java/com/example/animationlottie/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.animationlottie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
+    private  var islog = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
-}
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu , menu)
+        return true
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.login-> {return true}
+            R.id.logout-> {return true}
+            R.id.search -> {
+
+                val intent = Intent(this, MenuS::class.java)
+                this.startActivity(intent)
+
+                return true
+
+            }
+            else -> false
+        }}
+
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        var login =menu?.findItem(R.id.login)
+        var logout =menu?.findItem(R.id.logout)
+        if (islog) {
+            islog = !islog
+            login?.setVisible(true)
+            logout?.setVisible(false)
+        }else{
+            islog = !islog
+            login?.setVisible(false)
+            logout?.setVisible(true)
+
+        }
+        return true
+
+
+
+    }}
+
+
